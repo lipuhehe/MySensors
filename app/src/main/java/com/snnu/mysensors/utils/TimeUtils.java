b0d1334a6b91a399a536b5940e09235af5c2334b
lipu <1505384348@qq.com>
2021-08-25 10:27:27 +0800

    功能调整

--- app/src/main/java/com/snnu/mysensors/utils/TimeUtils.java
+++ app/src/main/java/com/snnu/mysensors/utils/TimeUtils.java
@@ -5,11 +5,10 @@ import java.util.Date;
 
 public class TimeUtils {
     public static String getTime() {
-        long time = System.currentTimeMillis();//long now = android.os.SystemClock.uptimeMillis();
+        //long time = System.currentTimeMillis();//long now = android.os.SystemClock.uptimeMillis();
         SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:SS");
-        Date d1 = new Date(time);
-        String t1 = format.format(d1);
-        return t1;
+        Date current = new Date();
+        String currentTime = format.format(current);
+        return currentTime;
     }
-
 }
